b3923feb22f514c1b1a803583311163b26b0b29f
CrazyMax <crazy-max@users.noreply.github.com>
2022-04-24 17:08:56 +0200

    update dev dependencies and workflow

--- __tests__/containerd.test.ts
+++ __tests__/containerd.test.ts
@@ -1,16 +1,15 @@
-import fs = require('fs');
+import {describe, expect, it} from '@jest/globals';
+import * as fs from 'fs';
 import * as containerd from '../src/containerd';
 
 describe('installer', () => {
   it('acquires v1.4.1 version of containerd', async () => {
     const install = await containerd.install('v1.4.1');
-    console.log(install);
     expect(fs.existsSync(install.cachePath)).toBe(true);
   }, 100000);
 
   it('acquires latest version of containerd', async () => {
     const install = await containerd.install('latest');
-    console.log(install);
     expect(fs.existsSync(install.cachePath)).toBe(true);
   }, 100000);
 });
